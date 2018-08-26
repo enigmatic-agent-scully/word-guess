e533952b00120e4f14bf75c25060459adcfa41fd
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-08-26 18:39:51 -0400

    main html coded

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+
+//source of randomly chosen word
+var words = ["pilot", "little green men", "squeeze", "tooms", "beyond the sea", "ebe", "jersey devil"];
+//sets one random string to var = word
+var word = words[Math.floor(Math.random() * words.length)];
+
+var answerArray = [];
+
+
+
+
