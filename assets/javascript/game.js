8fb390f12dbfb7bb9e3fadf5b810447c2a63c9f3
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-08-31 21:32:36 -0400

    step 4 out of 32468236...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,42 @@
 
-//source of randomly chosen word
-var words = ["pilot", "little green men", "squeeze", "tooms", "beyond the sea", "ebe", "jersey devil"];
-//sets one random string to var = word
-var word = words[Math.floor(Math.random() * words.length)];
+var words = ["scully", "mulder", "krycek", "deepthroat", "skinner", "kersh", "byers", "frohike", "langly", "dogget", "reyes",];
 
-var answerArray = [];
+var currentWordIndex = words[Math.floor(Math.random() * words.length)];
 
 
+var remainingGuesses = currentWordIndex.length;
+var wins = 0;
+var gameStarted = false;
+var hasFinished = false;
+var maxTries = 14;
+var answerArray = currentWordIndex.split("");
+
+console.log(currentWordIndex);
+console.log(currentWordIndex.length);
+console.log(answerArray);
+
+window.addEventListener("DOMContentLoaded", function loadGame () {
+    for (var i = 0; i < currentWordIndex.length; i++) {
+        var newSpan = document.createElement("span");
+        var textNode = document.createTextNode("_ ");
+        newSpan.appendChild(textNode);
+        document.getElementById("currentWord").appendChild(newSpan);
+    };
+    var winsSpan = document.createElement("span");
+    var winsText = document.createTextNode(wins);
+    winsSpan.appendChild(winsText);
+    document.getElementById("totalWins").appendChild(winsSpan);
+    var guessCountSpan = document.createElement("span");
+    var guessCountText = document.createTextNode(maxTries);
+    guessCountSpan.appendChild(guessCountText);
+    document.getElementById("guessesRemaining").appendChild(guessCountSpan);
+});
+
+//document.addEventListener("keypress", function uponGuess() {
+  //var keyPress = String.fromCharCode(event.keyCode || event.code).toLowerCase();
+  //var guessedLetters = [];
+  //keyPress.push(guessedLetters);
 
 
+
+//})
