210efa9ba720aac8db730e53fc96c846ba962811
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-09-16 18:44:11 -0400

    IT WORKS (mostly)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,6 @@ var words = ["scully", "mulder", "krycek", "deepthroat", "skinner", "kersh", "by
 var currentWord = words[Math.floor(Math.random() * words.length)]; // chooses random word
 var answerArray = currentWord.split(""); //splits chosen word into an array of letters
 
-var correctArray = [];
 var incorrectArray = [];
 var wins = 0;
 var gameStarted = false;
@@ -12,58 +11,49 @@ var hasFinished = false;
 var misses = 0;
 var spacesArray = [];
 var displayWord = document.getElementById("currentWord");
-
-
+var lettersGuessed = document.getElementById("guessedLetters");
 
 console.log(currentWord);
 console.log(currentWord.length);
 console.log(answerArray);
 
-window.addEventListener("DOMContentLoaded", function loadGame () {
-    console.log(spacesArray);
+window.addEventListener("DOMContentLoaded", function loadGame() {
+
     for (var i = 0; i < currentWord.length; i++) {
         spacesArray.push("_ ");
-
     };
+    console.log(spacesArray);
     displayWord.innerHTML = `<span>${spacesArray.join("")}<span>`;
 });
 
-
-
 window.onkeypress = function uponGuess(event) {
     gameStarted = true;
-    let userInput = event.key; 
-    var findIndex = answerArray.indexOf(userInput);
+    let userInput = event.key;
+    var index = answerArray.indexOf(userInput);
     console.log(userInput);
-    //for (i = 0; i < answerArray.length; i++) {
-        if (findIndex != -1) {
-            var regExp = new RegExp("/" + findIndex + "/", "g");
-            //console.log(regExp)
-            var x = spacesArray.join('').replace(spacesArray[findIndex],  userInput);
-            console.log(findIndex);
-            console.log(spacesArray[findIndex]);
-            console.log(x);
 
-            // spacesArray.join("").replace(spacesArray[findIndex], userInput);
-            // //spacesArray.splice((answerArray.indexOf(userInput)), 1, userInput);
-            // //spacesArray.replace(/`answerArray.indexOf(userInput)`/g)
-            // displayWord.innerHTML = `<span>${spacesArray.join("")}<span>`;
+    //for (i = 0; i < answerArray.length; i++) {
+    if (index != -1) {
+        console.log(index);
+        spacesArray.splice(index, 1, userInput);
+        index2 = answerArray.indexOf(userInput, index + 1);
+        if (index2 != -1) {
+            spacesArray.splice(index2, 1, userInput);
+            console.log(index2);
+            displayWord.innerHTML = `<span>${spacesArray.join(" ")}<span>`;
         }
         else {
-            console.log("Try again");
+            displayWord.innerHTML = `<span>${spacesArray.join(" ")}<span>`;
         }
-        //if (event.key !== answerArray[i]) {
+        console.log(spacesArray);
+    }
+    else {
+        console.log("Try again");
+        incorrectArray.push(userInput);
+        lettersGuessed.innerHTML = `<span>Letters Guessed: ${incorrectArray.join(", ")}</span>`
+    }
+}
 
-            //spacesArray.push(userInput);
-           // misses--;
-           // var guessedLettersSpan = document.createElement("span");
-           // var guessedLettersText = document.createTextNode(spacesArray);
-          //  guessedLettersSpan.appendChild(guessedLettersText);
-           // document.getElementById("guessedLetters").appendChild(guessedLettersSpan);
-           // break;
-        }
-    
-    //};
 
 
 
